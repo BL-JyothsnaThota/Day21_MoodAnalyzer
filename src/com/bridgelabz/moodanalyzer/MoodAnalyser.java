14d169f658a3e16d657ba05e2534c8f05e064e8c
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2026-05-22 17:33:23 +0530

    New test cases after adding constructor

--- src/com/bridgelabz/moodanalyzer/MoodAnalyser.java
+++ src/com/bridgelabz/moodanalyzer/MoodAnalyser.java
@@ -1,7 +1,19 @@
 package com.bridgelabz.moodanalyzer;
 public class MoodAnalyser {
 
-    public String analyseMood(String message) {
+    private String message;
+
+    // Default Constructor
+    public MoodAnalyser() {
+    }
+
+    // Parameterized Constructor
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
+    // No parameter method
+    public String analyseMood() {
         if (message.contains("Sad")) {
             return "SAD";
         }
